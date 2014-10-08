a05c359370a20530fe09afeac26c27aa8498d1f8
Alistair Broomhead <alistair.broomhead@gmail.com>
2014-10-08 17:10:16 +0100

    Keep a list of all the created buses

--- bus_listeners.js
+++ bus_listeners.js
@@ -11,38 +11,45 @@ tau.mashups
   .addModule('BusListeners', function(reg){
 
     function Listener(bus, event, func){
-      this.bus = bus;
-      this.event = event;
-      this.func = func;
+      this._bus_name = bus;
+      this._event_name = event;
+      this._func = func;
     }
 
-    function BusListeners(){
-      var listeners = [];
-
-      this.Add = function Add(bus, event, func){
-        listeners.push(new Listener(bus, event, func));
-      };
-
-      reg.on('create', function(e, data) {
-        for (var i = 0; i < listeners.len; i++){
-          var listener = listeners[i];
-          var bus = data.bus;
-          if (bus.name === listener.bus) {
-            bus.on(listener.event, listener.func);
-          }
+    Listener.prototype.bind = function bind(data){
+        var bus = data.bus;
+        if (bus.name === this._bus_name){
+            bus.on(this._event_name, this._func);
         }
-      });
+    };
 
-      reg.on('destroy', function(e, data) {
-        for (var i = 0; i < listeners.len; i++){
-          var listener = listeners[i];
-          var bus = data.bus;
-          if (bus.name === listener.bus) {
-            bus.removeListener(listener.event, listener.func);
-          }
+    Listener.prototype.unbind = function unbind(data){
+        var bus = data.bus;
+        if (bus.name === this._bus_name){
+            bus.removeListener(this._event_name, this._func);
         }
-      });
-    }
+    };
 
-    return new BusListeners();
+    var BusListeners = {
+        listeners: [],
+
+        Add: function Add(bus, event, func){
+            this.listeners.push(new Listener(bus, event, func));
+        }
+    };
+    document.created = [];
+
+    reg.on('create', function (e, data){
+        document.created.push(data);
+        for (var i = 0; i < BusListeners.listeners.length; i++){
+            BusListeners.listeners[i].bind(data);
+        }
+    });
+    reg.on('destroy', function (e, data){
+        for (var i = 0; i < BusListeners.listeners.length; i++){
+            BusListeners.listeners[i].unbind(data);
+        }
+    });
+    document.BusListeners = BusListeners;
+    return BusListeners;
   });
